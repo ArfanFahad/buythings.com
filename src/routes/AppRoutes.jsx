292283627c1d836f47a56f7f0bc5e359d961cc97
Arfan Fahad <arfanfahad40@gmail.com>
2025-07-30 12:22:32 +0600

    feat: define app routing with nested MainLayout and AdminLayout routes

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,36 @@
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/shop/HomePage";
+import ProductPage from "../pages/shop/ProductPage";
+import AboutPage from "../pages/shop/AboutPage";
+import ContactPage from "../pages/shop/ContactPage";
+import AdminLayout from "../layouts/AdminLayout";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CategoriesPage from "../pages/admin/CategoriesPage";
+import ProductPageAdmin from "../pages/admin/ProductPageAdmin";
+import OrdersPage from "../pages/admin/OrdersPage";
+import UsersPage from "../pages/admin/UsersPage";
+import SettingsPage from "../pages/admin/SettingsPage";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<ProductPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Route>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="category" element={<CategoriesPage />} />
+        <Route path="productpage" element={<ProductPageAdmin />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="users" element={<UsersPage />} />
+        <Route path="setting" element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default AppRoutes;
