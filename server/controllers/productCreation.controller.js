8911c88d39523ad79b7cd6cea7ecd674560a55c4
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-02 01:05:46 +0600

    fix(api): validate and parse product creation input

--- server/controllers/productCreation.controller.js
+++ server/controllers/productCreation.controller.js
@@ -2,22 +2,33 @@ import { createProduct } from "../product/product.create.js";
 
 export const productCreation = async (req, res) => {
   try {
-    const { name, description, price, stock, imageUrl } = req.body;
+    const { name, description } = req.body;
+    const price = parseFloat(req.body.price);
+    const stock = parseInt(req.body.stock);
+    // generating image URL
+    const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
 
     if (!name || !description || !price || !stock || !imageUrl) {
       return res.status(400).json({ error: "All Fields are required" });
     }
 
-    const newProduct = await createProduct(
-      name,
-      description,
-      price,
-      stock,
-      imageUrl
-    );
+    try {
+      const newProduct = await createProduct(
+        name,
+        description,
+        price,
+        stock,
+        imageUrl
+      );
 
-    return res.status(200).json({ newProduct });
+      return res.status(200).json({ product: newProduct });
+    } catch (error) {
+      console.error("Error creating products: ", error.message);
+      return res.status(500).json({ error: "Database Error" });
+    }
   } catch (error) {
-    return res.status(500).json({ "Server Error": error.message });
+    return res
+      .status(500)
+      .json({ error: "Server Error", details: error.message });
   }
 };
