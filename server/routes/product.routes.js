95618f2d0c2627ad7c1020571892acf5e59ca211
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-02 01:07:22 +0600

    feat(product): implement image file uploads with Multer

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,10 +1,28 @@
 import express from "express";
 import { productCreation } from "../controllers/productCreation.controller.js";
 import { getAllProducts } from "../controllers/productGet.controller.js";
+import { fileConfiguration } from "../utils/fileConfig.js";
+import multer from "multer";
+
 const router = express.Router();
 
+router.use("/uploads", express.static(fileConfiguration.uploadFolderRoute));
+
+// Setup multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, fileConfiguration.uploadFolderRoute);
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 // Product Creation
-router.post("/create", productCreation);
+router.post("/create", upload.single("imageFile"), productCreation);
 
 // Getting Product
 router.get("/allProducts", getAllProducts);
