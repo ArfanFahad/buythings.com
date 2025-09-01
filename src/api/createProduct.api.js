319567c1dec7c54d239005fa3ebd55a5dd923b04
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-02 01:10:23 +0600

    feat(product): implement end-to-end image file uploads

--- src/api/createProduct.api.js
+++ src/api/createProduct.api.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:3000/v1/product/create";
 
-export const createProduct = async (productData) => {
+export const createProduct = async (formData) => {
   try {
-    const res = await axios.post(`${API_BASE_URL}`, productData);
+    const res = await axios.post(`${API_BASE_URL}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log("Response in Frontend: ", res.data);
     return res.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Product Creation Failed");
