816c0a63f243082beb7ea1b46da9134c173d05cb
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-03 12:13:19 +0600

    feat: add product deletion handler with confirmation and error handling

--- src/pages/admin/ProductPageAdmin.jsx
+++ src/pages/admin/ProductPageAdmin.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { fetchAllProducts } from "../../api/getProduct.api.js";
+import { deleteProduct } from "../../api/deleteProduct.api.js";
 
 export default function ProductPageAdmin() {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const data = await fetchAllProducts();
         setProduct(data);
-        // This is manual timing as API data
+        // API Timing Simulation
         setTimeout(() => {
           setLoading(false);
         }, 1000);
       } catch (error) {
         console.error("Error fetching data: ", error.message);
+        setError(error.message);
         setLoading(false);
       }
     };
@@ -24,8 +27,25 @@ export default function ProductPageAdmin() {
     fetchProducts();
   }, []);
 
-  if (loading)
+  const handleDelete = async (id) => {
+    if (!window.confirm("Are you sure you want to delete this product?"))
+      return;
+
+    try {
+      await deleteProduct(id);
+      setProduct(product.filter((p) => p.id !== id));
+    } catch (error) {
+      console.error("Delete Failed: ", error.message);
+    }
+  };
+
+  if (loading) {
     return <p className="text-white font-light text-2xl">Loading...</p>;
+  }
+
+  if (error) {
+    return <p className="text-red-500 font-light text-3xl">Error: {error}</p>;
+  }
 
   return (
     <div>
@@ -52,6 +72,7 @@ export default function ProductPageAdmin() {
         <tbody>
           {product.map((myProduct) => (
             <tr key={myProduct.id} className="hover:bg-white/10">
+              {console.log(myProduct.id)}
               <td className="px-4 py-2">{myProduct.name}</td>
               <td className="px-4 py-2">{myProduct.description}</td>
               <td className="px-4 py-2">{myProduct.price}</td>
@@ -62,7 +83,10 @@ export default function ProductPageAdmin() {
                 <button className="mr-1 bg-yellow-500 px-2 py-1 cursor-pointer">
                   Edit
                 </button>
-                <button className="ml-1 bg-red-500 px-2 py-1 text-white cursor-pointer">
+                <button
+                  className="ml-1 bg-red-500 px-2 py-1 text-white cursor-pointer"
+                  onClick={() => handleDelete(myProduct.id)}
+                >
                   Delete
                 </button>
               </td>
