6b5e31170d5e46d5dc8e0210e847c032baad371f
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-04 11:17:29 +0600

    feat(updateProduct.api): add updateProduct API integration

---
+++ src/api/updateProduct.api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_BASE_EDIT = "http://localhost:3000/v1/product";
+
+export const updateProduct = async (id, formData) => {
+  try {
+    const response = await axios.put(`${API_BASE_EDIT}/${id}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to update product"
+    );
+  }
+};
