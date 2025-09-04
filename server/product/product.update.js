5833ed53f386971173365195b743666d8b9d5d6c
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-04 11:10:17 +0600

    feat: update product with latest changes

--- server/product/product.update.js
+++ server/product/product.update.js
@@ -1,18 +1,16 @@
 import prisma from "../db/client.js";
 
 // Updating product
-export const updateProduct = async () => {
+export const updatedProduct = async (id) => {
   try {
     const data = await prisma.product.findUnique({
       where: {
-        id: "53d976f7-85dc-4371-a19e-98095c6eba88",
+        id: id,
       },
     });
-    return console.log(data);
+    return console.log("Data got updated: ", data);
   } catch (error) {
     console.error("Error updating product: ", error.message);
     throw error;
   }
 };
-
-updateProduct();
