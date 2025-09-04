d5b200b25506edf3ed254ed232004db1543f33d4
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-04 11:11:28 +0600

    refactor(routes): use shared multerConfig, add update route, and clean up product routes

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,31 +2,22 @@ import express from "express";
 import { productCreation } from "../controllers/productCreation.controller.js";
 import { getAllProducts } from "../controllers/productGet.controller.js";
 import { fileConfiguration } from "../utils/fileConfig.js";
+import { updateProduct } from "../controllers/productUpdate.controller.js";
 import { productDelete } from "../controllers/productDelete.controller.js";
-import multer from "multer";
+import { upload } from "../utils/multerConfig.js";
 
 const router = express.Router();
 
 router.use("/uploads", express.static(fileConfiguration.uploadFolderRoute));
-
-// Setup multer storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, fileConfiguration.uploadFolderRoute);
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 // Product Creation
 router.post("/create", upload.single("imageFile"), productCreation);
 
 // Getting Product
 router.get("/allProducts", getAllProducts);
+
+// Edit Product
+router.put("/update/:id", upload.single("imageFile"), updateProduct);
 
 // Deleting Product
 router.delete("/remove/:id", productDelete);
