21f8d0df26f9ee9232ecf8214de383527060a410
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-06 01:23:49 +0600

    refactor(server): omit file uploading configuration

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { productCreation } from "../controllers/productCreation.controller.js";
 import { getAllProducts } from "../controllers/productGet.controller.js";
-import { fileConfiguration } from "../utils/fileConfig.js";
 import { updateProduct } from "../controllers/productUpdate.controller.js";
 import { productDelete } from "../controllers/productDelete.controller.js";
 import { upload } from "../utils/multerConfig.js";
+import { getProductById } from "../controllers/productGetById.controller.js";
 
 const router = express.Router();
-
-router.use("/uploads", express.static(fileConfiguration.uploadFolderRoute));
 
 // Product Creation
 router.post("/create", upload.single("imageFile"), productCreation);
 
 // Getting Product
 router.get("/allProducts", getAllProducts);
+
+// Get Single Product by ID for Editing Purpose
+router.get("/:id", getProductById);
 
 // Edit Product
 router.put("/update/:id", upload.single("imageFile"), updateProduct);
