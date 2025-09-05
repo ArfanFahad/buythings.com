2b7b54f9ab11414cf755093a176b22c625622971
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-06 01:20:42 +0600

    feat(product): add controller to fetch product by ID

---
+++ server/controllers/productGetById.controller.js
@@ -0,0 +1,18 @@
+import { gettingProductById } from "../product/product.getById.js";
+
+export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const getData = await gettingProductById(id);
+
+    if (!getData) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json({ getData });
+  } catch (error) {
+    console.error("Error fetching product by ID: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
