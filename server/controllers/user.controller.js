e66157ff075d279b52e0010853b55f37593061b4
Arfan Fahad <arfanfahad40@gmail.com>
2025-09-13 00:51:46 +0600

    feat(user.controller): implement and test signup controller for user register

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,3 +9,12 @@ export const getUsers = async (req, res) => {
     return res.status(500).json({ "Error Getting User Data": error.message });
   }
 };
+
+export const userCreation = async (req, res) => {
+  try {
+    const bdy = req.body;
+    return res.status(200).json({ bdy });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
