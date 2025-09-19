03d9ad38a37cac3c364de9983af138266cb3d46f
ArfanFahad <arfanfahad40@gmail.com>
2025-09-20 00:46:23 +0600

    refactor(auth.routes): import & include users auth level file

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { userRegistration } from "../controllers/userController/userCreation.controller.js";
+import { userLogin } from "../controllers/userController/userLogin.controller.js";
+
 const router = express.Router();
 
 // User Registration
-router.post("/register", registerUser);
+router.post("/register", userRegistration);
+
+// User Login
+router.post("/login", userLogin);
 
 export default router;
