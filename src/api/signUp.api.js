dab16a0b5a077a9339cff6a3098108005b0b1ed0
ArfanFahad <arfanfahad40@gmail.com>
2025-09-30 10:36:11 +0600

    Update signUp API implementation

--- src/api/signUp.api.js
+++ src/api/signUp.api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const API_BASE_SIGNUP = "http://localhost:3000/v1/users/signup";
+const API_BASE_SIGNUP = "http://localhost:3000/v1/auth/register";
 
 export const userSignUp = async (signUpData) => {
   try {
-    const response = await axios.post(API_BASE_SIGNUP, signUpData);
+    const response = await axios.post(API_BASE_SIGNUP, signUpData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Failed to create user");
