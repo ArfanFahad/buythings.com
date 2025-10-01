bc484ed3a1dfab455737240f753f40f077c2ae24
ArfanFahad <arfanfahad40@gmail.com>
2025-10-02 00:18:31 +0600

    feat(userLoginPage): update userLogin

--- src/pages/shop/LoginPage.jsx
+++ src/pages/shop/LoginPage.jsx
@@ -1,20 +1,55 @@
+import { userLogin } from "../../api/login.api";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
+  // login state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    const loginData = {
+      email: email,
+      password: password,
+    };
+
+    console.log("Login Data: ", loginData);
+
+    // api call
+    try {
+      const data = await userLogin(loginData);
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    // reset form
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center bg-gray-400">
       <div className="w-5/12 p-20 mt-10">
         <h2 className="text-3xl mb-10 font-extrabold text-center text-gray-800">
           Login <span className="text-indigo-500"> buythings.com</span>
         </h2>
 
-        <form className="space-y-5">
+        <form className="space-y-5" method="POST" onSubmit={userLoginSubmit}>
           <div>
             <label className="block text-gray-700 font-medium mb-1">
               Email:{" "}
             </label>
             <input
               type="email"
+              name="email"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full px-4 py-3 border rounded outline-0"
+              required
             />
           </div>
 
@@ -23,9 +58,13 @@ export default function LoginPage() {
               Password:
             </label>
             <input
-              type="email"
+              type="password"
+              name="password"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full px-4 py-3 border rounded outline-0"
+              required
             />
             <p className="mt-2 text-right cursor-pointer">
               <a href="http://">Forget Password?</a>
