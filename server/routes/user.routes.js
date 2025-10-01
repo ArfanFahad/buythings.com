f2cc33f3d2e48475119d2a65165f9f91f03f3860
ArfanFahad <arfanfahad40@gmail.com>
2025-10-02 00:22:27 +0600

    refactor(userController): naming convention change

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getUsers } from "../controllers/userController/getAllUsers.controller.js";
+import { getUsersController } from "../controllers/userController/getAllUsers.controller.js";
+import { userDeleteController } from "../controllers/userController/userDeletion.controller.js";
 
 const router = express.Router();
 
 // Get All Users
-router.get("/all", getUsers);
+router.get("/all", getUsersController);
+
+// Delete Single User
+router.delete("/:id", userDeleteController);
 
 export default router;
