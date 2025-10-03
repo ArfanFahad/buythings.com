d1c44e6550997fd25d5530c6c5f0a983d8129021
ArfanFahad <arfanfahad40@gmail.com>
2025-10-03 23:01:59 +0600

    feat: add middleware (needs improvement)

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,13 +1,18 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 
-export const protectedUser = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+export const protectedAdmin = (req, res, next) => {
+  const authHeader = req.headers;
+  console.log(authHeader);
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Not authorized, token missing" });
-  }
+  // const authHeader = req.headers.authorization;
 
-  const token = authHeader.split(" ")[1];
-  console.log(token);
+  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  //   return res.status(401).json({ message: "Not authorized, token missing" });
+  // }
+
+  // // Extracting From Header
+  // const token = authHeader.split(" ")[1];
+  // console.log("Token from frontend: ", token);
+  // console.log(token);
 };
