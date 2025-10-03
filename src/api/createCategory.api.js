d19c00a3071d2f14b08948daf64dc551e5832e35
ArfanFahad <arfanfahad40@gmail.com>
2025-10-03 22:46:48 +0600

    feat(create category): category creation api add

---
+++ src/api/createCategory.api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_CREATE_CATEGORY_URL = "http://localhost:3000/v1/categories/create";
+
+export const createCategoryAPI = async (categoryData) => {
+  try {
+    const res = await axios.post(`${API_CREATE_CATEGORY_URL}`, categoryData);
+    console.log("Response: ", res.data);
+    return res.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Category Creation Failed"
+    );
+  }
+};
