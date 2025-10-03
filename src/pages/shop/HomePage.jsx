3472d70e8227ece56e7ed8bb4885ec34fc24db04
ArfanFahad <arfanfahad40@gmail.com>
2025-10-03 22:55:47 +0600

    feat(shop): add HomePage with shop navigation and layout

--- src/pages/shop/HomePage.jsx
+++ src/pages/shop/HomePage.jsx
@@ -1,10 +1,26 @@
+import heroImage from "../../assets/heroImage.jpg";
+import Mobile from "./HomePage/Mobile";
 export default function HomePage() {
   return (
     <>
       <div className="p-6">
+        <div className="relative mb-8">
+          <img
+            src={heroImage}
+            alt="Hero"
+            className="w-full h-64 object-cover rounded-2xl shadow-lg"
+          />
+          {/* text overlay */}
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-white text-3xl md:text-5xl font-bold drop-shadow-lg">
+              Welcome to our store
+            </h1>
+          </div>
+        </div>
         <div className="">
           <div>
             <h2>Mobile</h2>
+            <Mobile />
           </div>
 
           <div>
