e8abc4fd399c8b7463a568a3c16f388f4ae0ac45
ArfanFahad <arfanfahad40@gmail.com>
2025-10-03 22:48:37 +0600

    style(admin): improve CategoriesPage layout and UI

--- src/pages/admin/CategoriesPage.jsx
+++ src/pages/admin/CategoriesPage.jsx
@@ -1,7 +1,44 @@
+import { NavLink } from "react-router-dom";
+
 export default function CategoriesPage() {
   return (
-    <>
-      <h1>This is Category Page</h1>
-    </>
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-semibold">Categories</h1>
+
+        <NavLink
+          to="add"
+          className="inline-block mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
+        >
+          + Add Categories
+        </NavLink>
+      </div>
+
+      <table className="min-w-full text-sm text-left">
+        <thead className="bg-white/10 text-gray-700 p-4">
+          <tr>
+            <th className="px-4 py-2">Category Name</th>
+            <th className="px-4 py-2">Slug Name</th>
+            <th className="px-4 py-2">Created at</th>
+            <th className="px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="px-4 py-2">Mobile</td>
+            <td className="px-4 py-2">mobile</td>
+            <td className="px-4 py-2">2025-10-01</td>
+            <td className="px-4 py-2">
+              <NavLink className="mr-1 bg-yellow-500 px-2 py-1 cursor-pointer">
+                Edit
+              </NavLink>
+              <button className="ml-1 bg-red-500 px-2 py-1 text-white cursor-pointer">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
