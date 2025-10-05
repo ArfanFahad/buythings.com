e944eaa843ed925d0d65e2982987c137d94a3bc2
ArfanFahad <arfanfahad40@gmail.com>
2025-10-05 11:17:24 +0600

    feat(category): add categories routes logic

--- server/routes/categoryRoutes/categories.routes.js
+++ server/routes/categoryRoutes/categories.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { categoryCreationController } from "../../controllers/categoryController/categoryCreation.controller.js";
+import { getCategoryController } from "../../controllers/categoryController/getCategory.controller.js";
 
 const router = express.Router();
 
-// User Creation Route
+// Getting Category
+router.get("/", getCategoryController);
+
+// Category Creation Route
 router.post("/create", categoryCreationController);
 
 export default router;
