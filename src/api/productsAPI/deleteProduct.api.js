6a2ace77b9e0e36d4e0444532b74d59746a75a1c
ArfanFahad <arfanfahad40@gmail.com>
2025-10-17 22:30:58 +0600

    feat(api): add deleteProduct API

--- src/api/productsAPI/deleteProduct.api.js
+++ src/api/productsAPI/deleteProduct.api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_PRODUCT_DELETE = "http://localhost:3000/v1/product/remove";
+const API_PRODUCT_DELETE = "http://localhost:3000/v1/products/remove";
 
 export const deleteProduct = async (id) => {
   try {
     const response = await axios.delete(`${API_PRODUCT_DELETE}/${id}`);
-    console.log("Frontend Sending 'id' that will be deleted");
+    console.log("Deleted Id:", id);
     return response.data;
   } catch (error) {
     console.error("Failed to delete product: ", error);
