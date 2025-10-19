3e13ea9628193ca931f799575b35f9d001cf89cd
ArfanFahad <arfanfahad40@gmail.com>
2025-10-19 13:22:20 +0600

    feat(product): update product creation logic

--- server/product/product.create.js
+++ server/product/product.create.js
@@ -3,11 +3,17 @@ import prisma from "../db/client.js";
 export const createProduct = async (
   name,
   description,
+  categoryId,
   price,
   stock,
   imageUrl
 ) => {
-  return await prisma.product.create({
-    data: { name, description, price, stock, imageUrl },
-  });
+  try {
+    const productData = await prisma.product.create({
+      data: { name, description, categoryId, price, stock, imageUrl },
+    });
+    return productData;
+  } catch (error) {
+    console.error("Failed creating product: ", error.message);
+  }
 };
