4b6c50a8b736d94b890d14a6b65e3919bd19bf1b
ArfanFahad <arfanfahad40@gmail.com>
2025-10-19 13:22:48 +0600

    feat(product): update product retrieval logic

--- server/product/product.get.js
+++ server/product/product.get.js
@@ -3,7 +3,15 @@ import prisma from "../db/client.js";
 // Getting all products
 export const getAllProductsData = async () => {
   try {
-    const data = await prisma.product.findMany();
+    const data = await prisma.product.findMany({
+      include: {
+        category: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
     return data;
   } catch (error) {
     console.error("Error fetching products: ", error.message);
