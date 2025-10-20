481d2f91a8e06ad5c6044d1070ad6ed68932b301
ArfanFahad <arfanfahad40@gmail.com>
2025-10-20 12:44:42 +0600

    feat: add frontend updateCategoryAPI

---
+++ server/controllers/categoryController/updateCategory.controller.js
@@ -0,0 +1,20 @@
+import { updateCategoryModel } from "../../model/category/updateCategory.model.js";
+
+export const updateCategoryController = async (req, res) => {
+  const { id } = req.params;
+  const { categoryName, categorySlug } = req.body;
+
+  // Creating Object
+  const updatedCategory = {
+    id,
+    categoryName,
+    categorySlug,
+  };
+
+  try {
+    const update = await updateCategoryModel(id, updatedCategory);
+    return update;
+  } catch (error) {
+    console.error("Error updating product: ", error.message);
+  }
+};
