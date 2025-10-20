72d8b695ebaa58be46b2b4c843c90a30ecab4f61
ArfanFahad <arfanfahad40@gmail.com>
2025-10-20 12:56:40 +0600

    feat: add updateCategory frontend api

---
+++ src/api/categoriesAPI/updateCategory.api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_BASE_UPDATE = "http://localhost:3000/v1/categories/update";
+
+export const updateCategory = async (id, category) => {
+  try {
+    const response = await axios.put(`${API_BASE_UPDATE}/${id}`, category);
+    // return response.data;
+    console.log(response);
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to update category"
+    );
+  }
+};
