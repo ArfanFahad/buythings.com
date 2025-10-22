ea72beb553a5530561c4c32005f6c4d324980132
ArfanFahad <arfanfahad40@gmail.com>
2025-10-22 11:22:19 +0600

    refactor: omit unnecessary log

--- src/api/categoriesAPI/updateCategory.api.js
+++ src/api/categoriesAPI/updateCategory.api.js
@@ -5,8 +5,7 @@ const API_BASE_UPDATE = "http://localhost:3000/v1/categories/update";
 export const updateCategory = async (id, category) => {
   try {
     const response = await axios.put(`${API_BASE_UPDATE}/${id}`, category);
-    // return response.data;
-    console.log(response);
+    return response.data;
   } catch (error) {
     throw new Error(
       error.response?.data?.message || "Failed to update category"
